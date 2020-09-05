0e93f3129a0bd93288a92913d0b1a562bae0bbbd
Bruno Cabral <cabral.brc@gmail.com>
2020-09-04 23:23:27 -0700

    asteroid created, change angular drag to 0

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+    public float tumble;
+
+    private new Rigidbody rigidbody;
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        rigidbody.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+}
