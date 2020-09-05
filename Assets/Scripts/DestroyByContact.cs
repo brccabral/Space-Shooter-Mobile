43bf10233cba1a2bc0b52962f9ca2d10de070845
Bruno Cabral <cabral.brc@gmail.com>
2020-09-04 23:30:05 -0700

    the shot destroys the asteroid and it doesn't get destroyed by boundary

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Boundary"))
+            return;
+
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
