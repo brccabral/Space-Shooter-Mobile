1231b50698cb335e6719440dd829c17fd242c0dd
Bruno Cabral <cabral.brc@gmail.com>
2020-09-05 02:51:58 -0700

    background scrolls

---
+++ Assets/Scripts/BGScroller.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class BGScroller : MonoBehaviour
+{
+    public float scrollSpeed;
+    public float tileSizeZ; // same as Quad Scale Y in the background transformation
+    private void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+
+        transform.position = Vector3.forward * newPosition;
+    }
+}
