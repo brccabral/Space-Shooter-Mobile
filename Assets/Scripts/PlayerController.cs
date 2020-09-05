144d252c76176fe81ab423c7f0847321947981c6
Bruno Cabral <cabral.brc@gmail.com>
2020-09-04 22:39:44 -0700

    player can move and it tilts

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Schema;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float tiltX;
+    public float tiltZ;
+    public Boundary boundary;
+    private new Rigidbody rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rigidbody.velocity = movement * speed;
+
+        rigidbody.position = new Vector3(
+            Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax));
+
+        rigidbody.rotation = Quaternion.Euler(rigidbody.velocity.z * tiltX, 0.0f, rigidbody.velocity.x * -tiltZ);
+    }
+}
+
+[Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
