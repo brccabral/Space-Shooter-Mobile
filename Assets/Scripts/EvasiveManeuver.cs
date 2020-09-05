9706e5ffdc0eaeab7bc12789a425d87129a9a864
Bruno Cabral <cabral.brc@gmail.com>
2020-09-05 13:24:23 -0700

    enemy moves left and right

---
+++ Assets/Scripts/EvasiveManeuver.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvasiveManeuver : MonoBehaviour
+{
+    public float dodge;
+    public float smoothing;
+    public float tiltZ;
+    public Vector2 startWait;
+    public Vector2 maneuverTime;
+    public Vector2 maneuverWait;
+    public Boundary boundary;
+
+    private float targetManeuver;
+    private float currentSpeed;
+
+    private new Rigidbody rigidbody;
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSpeed = rigidbody.velocity.z;
+        StartCoroutine(Evade());
+    }
+
+    private void FixedUpdate()
+    {
+        float newManeuver = Mathf.MoveTowards(rigidbody.velocity.x, targetManeuver, Time.deltaTime * smoothing);
+        rigidbody.velocity = new Vector3(newManeuver, 0.0f, currentSpeed);
+
+        rigidbody.position = new Vector3(
+            Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax));
+
+        rigidbody.rotation = Quaternion.Euler(0.0f, 0.0f, rigidbody.velocity.x * -tiltZ);
+    }
+
+    IEnumerator Evade()
+    {
+        yield return new WaitForSeconds(Random.Range(startWait.x, startWait.y));
+        while (true)
+        {
+            targetManeuver = Random.Range(1, dodge) * -Mathf.Sign(transform.position.x);
+            yield return new WaitForSeconds(Random.Range(maneuverTime.x, maneuverTime.y));
+            targetManeuver = 0;
+            yield return new WaitForSeconds(Random.Range(maneuverWait.x, maneuverWait.y));
+        }
+    }
+}
+
