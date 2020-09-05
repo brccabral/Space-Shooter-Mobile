0c7ebabe3daf9759128c3e1f07f05dcbbe63fbd8
Bruno Cabral <cabral.brc@gmail.com>
2020-09-04 23:50:29 -0700

    asteroid instantiate explosion when shot and when player touches it

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -2,10 +2,18 @@
 
 public class DestroyByContact : MonoBehaviour
 {
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Boundary"))
             return;
+
+        Instantiate(explosion, transform.position, transform.rotation);
+
+        if (other.CompareTag("Player"))
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
 
         Destroy(other.gameObject);
         Destroy(gameObject);
