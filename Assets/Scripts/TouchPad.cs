bb7a06b806b65051fa7333f618904aaa15e49902
Bruno Cabral <cabral.brc@gmail.com>
2020-09-06 01:20:01 -0700

    touches from the phone are detected by unity in Movement Zone canvas

---
+++ Assets/Scripts/TouchPad.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TouchPad : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+    private Vector2 origin;
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        // Set our start finger point
+        origin = eventData.position;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        // Get new finger position
+        Vector2 currentPosition = eventData.position;
+        Vector2 directionRaw = currentPosition - origin;
+        Vector2 direction = directionRaw.normalized;
+        Debug.Log(direction);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        // Reset everything
+    }
+}
